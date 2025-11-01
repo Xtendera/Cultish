2fd94dccd3ac0765be86806364e4a3b3fc27f306
xtendera <geektraindev@gmail.com>
2025-11-01 16:38:58 -0500

    Initial commit

---
+++ src/client/java/dev/ashrayshah/client/cultishClient.java
@@ -0,0 +1,10 @@
+package dev.ashrayshah.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class cultishClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/ashrayshah/cultish.java
@@ -0,0 +1,10 @@
+package dev.ashrayshah;
+
+import net.fabricmc.api.ModInitializer;
+
+public class cultish implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
